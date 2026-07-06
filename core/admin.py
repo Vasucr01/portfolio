e954eda7b86fe58fa115cea9329bffc1cc8727aa
Vasu Chauhan <vasucr01@gmail.com>
2026-07-06 11:26:49 +0530

    fix: update admin fields to match models for proper display

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 from .models import Profile, Skill, Project, Visitor
 
-admin.site.register(Profile)
-admin.site.register(Skill)
-admin.site.register(Project)
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('name', 'hero_title')
+    search_fields = ('name', 'about_text')
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ('name', 'percentage', 'order')
+    search_fields = ('name',)
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'order')
+    search_fields = ('title', 'description')
 
 @admin.register(Visitor)
 class VisitorAdmin(admin.ModelAdmin):
